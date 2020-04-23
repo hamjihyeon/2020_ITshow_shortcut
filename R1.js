5e73f3ba411945ded22289f413f36ec940145d69
hamjihyeon <48716412+hamjihyeon@users.noreply.github.com>
2020-04-23 23:01:45 +0900

    Add files via upload

---
+++ R1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class R1 extends React.Component {
+    
+
+    render() {
+        
+    
+
+        return (
+
+            <div>
+               
+            </div>
+        );
+    }
+}
+
+export default R1;
